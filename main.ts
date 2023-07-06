8709818c25011cbb8e40cb6fe9c49bdacb09d1f8
mostertjc <102650119+mostertjc@users.noreply.github.com>
2023-07-06 18:21:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.forever(function () {
+    basic.showString("JJTM Mixed info")
+})
+basic.forever(function () {
+    for (let i = 0; i <= 255; i++) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, i)
+    }
+    basic.pause(500)
+    for (let i = 0; i <= 255; i++) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Left, i)
+    }
+    basic.pause(2000)
+    for (let i = 0; i <= 255; i++) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, i)
+    }
+    basic.pause(2000)
+    for (let i = 0; i <= 255; i++) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, i)
+    }
+    basic.pause(500)
+})
